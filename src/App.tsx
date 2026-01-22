5a8c5aca0b4d1702f2d645b2252b4d2e44266695
NeslihanUTUK <nesli.utuk@gmail.com>
2026-01-22 15:53:28 +0100

    fiix: import

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,24 @@
-import './App.css'
-import { ColumnLayout, Checkbox as RhcCheckbox} from '@rijkshuisstijl-community/components-react';
-import '@rijkshuisstijl-community/design-tokens/dist/index.css';
-import '@rijkshuisstijl-community/components-css/dist/index.css';
+import "./App.css";
+import { Checkbox as RhcCheckbox } from "@rijkshuisstijl-community/components-react";
+import "@rijkshuisstijl-community/design-tokens/dist/index.css";
+
 import { Checkbox as UtrechtCheckbox } from "@utrecht/component-library-react";
-import { useState } from "react";
+import "@utrecht/component-library-css/dist/index.css";
 
 function App() {
-   const [isChecked, setIsChecked] = useState(true);
   return (
-    <>
-    <ColumnLayout>
-    <div className="checkbox-rhc">
-      <RhcCheckbox aria-label="checkbox-label" />
+    <div className="rhc-theme">
+      <div>
+        <label>
+          <UtrechtCheckbox
+            disabled
+          />
+            Remember me
+        </label>
+      </div>
+      
     </div>
-
-    <div className="utrecht-theme">
-      <label>
-        <UtrechtCheckbox 
-          checked={isChecked} 
-          onChange={(e) => setIsChecked(e.target.checked)} 
-        />
-        Remember me
-      </label>
-    </div>
-    </ColumnLayout>
-    </>
-  )
+  );
 }
 
 export default App;
